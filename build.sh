7b09b6653021e522671130f49529bc1348a7c083
john01dav <john01dav@gmail.com>
2016-12-28 01:43:59 -0600

    Added basic modules.yml parser

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #This script provides a fallback method to build MBS to allow it to be built on systems that do not yet have MBS or before MBS is able to build itself
-g++ --std=c++11 `ls ./main-module/src/*.cpp` -I./main-module/include -o mbs
+g++ --std=c++11 `ls ./main-module/src/*.cpp` -I./main-module/include -lyaml-cpp -o mbs
--- main-module/src/main.cpp
+++ main-module/src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <yaml-cpp/yaml.h>
 
 #include "log.h"
 #include "version.h"
 
 int main(int argc, char *argv[]){
   log(INFO, "Modular Build System Version " + VERSION);
+
+  //load modules
+  YAML::Node modulesFile = YAML::LoadFile("modules.yml");
+  YAML::Node modules = modulesFile["modules"];
+  for(auto it=modules.begin();it!=modules.end();++it){
+    std::cout << "Module Name: " << it->first << std::endl;
+    std::cout << "Source Path: " << it->second["src-dir"] << std::endl;
+    std::cout << "Include Path: " << it->second["include-dir"] << std::endl;
+
+    YAML::Node libraries = it->second["libraries"];
+    std::cout << "Libraries: " << std::endl;
+    for(auto lit=libraries.begin();lit!=libraries.end();++lit){
+      std::cout << "\t- " << lit->as<std::string>() << std::endl;
+    }
+  }
 }
