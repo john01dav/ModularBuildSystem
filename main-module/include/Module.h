b9fc02487e0f802a3d7e9231d9c2b59eacb9f863
john01dav <john01dav@gmail.com>
2016-12-28 02:07:53 -0600

    Refactored module parser to use module class/object

---
+++ main-module/include/Module.h
@@ -0,0 +1,27 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+#include <string>
+#include <vector>
+
+class Module{
+public:
+  Module(const std::string &name, const std::string &srcPath, const std::string &includePath)
+    : m_name(name), m_srcPath(srcPath), m_includePath(includePath) {}
+
+  const std::string& name() const { return m_name; }
+  const std::string& srcPath() const { return m_srcPath; }
+  const std::string& includePath() const { return m_includePath; }
+  const std::vector<std::string>& libraries() const { return m_libraries; }
+
+  std::vector<std::string>& libraries() { return m_libraries; }
+
+
+private:
+  std::string m_name;
+  std::string m_srcPath;
+  std::string m_includePath;
+  std::vector<std::string> m_libraries;
+};
+
+#endif
--- main-module/src/main.cpp
+++ main-module/src/main.cpp
@@ -1,24 +1,48 @@
 #include <iostream>
+#include <vector>
 #include <yaml-cpp/yaml.h>
 
 #include "log.h"
 #include "version.h"
+#include "Module.h"
 
 int main(int argc, char *argv[]){
   log(INFO, "Modular Build System Version " + VERSION);
+  log(INFO, "");
+
+  //setup data structures
+  std::vector<Module> modules;
 
   //load modules
   YAML::Node modulesFile = YAML::LoadFile("modules.yml");
-  YAML::Node modules = modulesFile["modules"];
-  for(auto it=modules.begin();it!=modules.end();++it){
-    std::cout << "Module Name: " << it->first << std::endl;
-    std::cout << "Source Path: " << it->second["src-dir"] << std::endl;
-    std::cout << "Include Path: " << it->second["include-dir"] << std::endl;
+  YAML::Node modulesNode = modulesFile["modules"];
+  for(auto it=modulesNode.begin();it!=modulesNode.end();++it){
+    Module module(
+      it->first.as<std::string>(),
+      it->second["src-dir"].as<std::string>(),
+      it->second["include-dir"].as<std::string>()
+    );
 
     YAML::Node libraries = it->second["libraries"];
-    std::cout << "Libraries: " << std::endl;
     for(auto lit=libraries.begin();lit!=libraries.end();++lit){
-      std::cout << "\t- " << lit->as<std::string>() << std::endl;
+      module.libraries().push_back(lit->as<std::string>());
     }
+
+    modules.push_back(std::move(module)); //can use std::move safely since this is the last statement in the loop
+  }
+
+  //print module list
+  for(Module &module : modules){
+    log(INFO, "Module Name: " + module.name());
+    log(INFO, "Source Path: " + module.srcPath());
+    log(INFO, "Include Path" + module.includePath());
+
+    std::string libraries = "Libraries: ";
+    for(const std::string &library : module.libraries()){
+      libraries += library + " ";
+    }
+
+    log(INFO, libraries);
+    log(INFO, "");
   }
 }
